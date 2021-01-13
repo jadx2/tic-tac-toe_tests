0f3a17fdb52be032439f4699bedf6a9d0013c61c
George <george.gbenle@outlook.com>
2021-01-13 12:13:53 -0500

    Create board display and verify moves

---
+++ lib/board.rb
@@ -0,0 +1,23 @@
+class Board
+    attr_reader :cells
+    def initialize
+        @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end
+
+    def display
+        puts <<-HERDOC
+                #{@cells[0]} | #{@cells[1]} | #{@cells[2]}
+                ---+---+---
+                #{@cells[3]} | #{@cells[4]} | #{@cells[5]}
+                ---+---+---
+                #{@cells[6]} | #{@cells[7]} | #{@cells[8]}
+        HERDOC
+    end
+
+    def verify_token(move)
+        until move.match(/[ 1-9 ]/)
+            puts 'Wrong move. Choose a number between 1 to 9'
+            move = gets.chomp
+        end
+    end
+end

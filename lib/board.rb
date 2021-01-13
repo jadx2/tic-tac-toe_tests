00348905701a7cc2507b9e372c5874f012490a54
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-01-13 12:37:31 -0500

    fix linter errors

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,24 @@
 class Board
-    attr_reader :cells
-    def initialize
-        @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    end
+  attr_reader :cells
 
-    def display
-        puts <<-HERDOC
+  def initialize
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def display
+    puts <<-HERDOC
                 #{@cells[0]} | #{@cells[1]} | #{@cells[2]}
                 ---+---+---
                 #{@cells[3]} | #{@cells[4]} | #{@cells[5]}
                 ---+---+---
                 #{@cells[6]} | #{@cells[7]} | #{@cells[8]}
-        HERDOC
-    end
+    HERDOC
+  end
 
-    def verify_token(move)
-        until move.match(/[ 1-9 ]/)
-            puts 'Wrong move. Choose a number between 1 to 9'
-            move = gets.chomp
-        end
+  def verify_token(move)
+    until move.match(/[ 1-9 ]/)
+      puts 'Wrong move. Choose a number between 1 to 9'
+      move = gets.chomp
     end
+  end
 end

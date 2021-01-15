c53ae1e48e48788c92395c84bc17a9304ffe2403
George <george.gbenle@outlook.com>
2021-01-15 12:38:42 -0500

    Create game logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,5 +1,6 @@
 class Board
   attr_reader :cells
+  WIN_COMBO = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
   def initialize
     @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -16,26 +17,38 @@ class Board
   end
 
   def update_board(input, symbol)
-    @cells[input.to_i - 1] = symbol
+    if input == @cells[input - 1]
+      @cells[input - 1] = symbol
+    else
+      false
+    end
   end
 
-  def verify_move(input, symbol)
-    cell_value = input.to_i - 1
-    if input.to_i > 9 || input.to_i < 1
-      puts "Choose between 1 and 9"
-    elsif @cells[cell_value] == "X" || @cells[cell_value] == "O"
-      puts "Space already taken. Try again"
-    else
-      update_board(input, symbol)
+  def winner(symbol)
+    WIN_COMBO.any? do |combo|
+      combo.all? { |item| @cells[item - 1] == symbol}
     end
+  end
+
+  def tie
+    @cells.all? { |cell| cell == 'X' || cell == 'O'}
   end
 end
 
-class Result
-  WIN_COMBO = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+class Game
 
-  def winner(player, input)
-    player1 = []
-    player2 = []
+  def verify_move(input, symbol, board)
+    cell_value = input - 1
+    if input.between?(1, 9) == false
+       puts "Choose between 1 and 9"
+       false
+    elsif board.update_board(input, symbol) == false
+      puts "Space already taken. Try again"
+      false
+    else
+      board.update_board(input, symbol)
+      true
+    end
   end
+
 end
